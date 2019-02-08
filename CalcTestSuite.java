adcc0d695fbf8ad0de3089fde2d2351a7191b2e7
Andrius Elsbergas <andrius.el@gmail.com>
2019-02-08 06:46:46 +0200

    Create CalcTestSuite.java

---
+++ CalcTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+	DivisionTest.class,
+	SumTest.class,
+	HelpTest.class,
+	CalculatorTest.class
+})
+
+public class CalcTestSuite{
+}
